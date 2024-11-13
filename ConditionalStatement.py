c639fe8481d2a7f04423514602619f6ac684beaa
AbuRahat889 <aburahatshaum889@gmail.com>
2024-11-13 21:11:59 +0600

    practic conditional statement

---
+++ ConditionalStatement.py
@@ -0,0 +1,13 @@
+
+# concatination
+
+firstName = "abu"
+lastName = " Rahat"
+
+name = firstName + lastName
+print(name)
+print(len(name))
+print(name[4])
+
+print(name[1:5]) #slicing
+print(name[1:len(name)]) #slicing
