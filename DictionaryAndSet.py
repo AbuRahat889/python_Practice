6d6415f1d33a7a242ed6ada22c4a9ea3f8f3926b
AbuRahat889 <aburahatshaum889@gmail.com>
2024-11-16 22:08:10 +0600

    lernint set in python

--- DictionaryAndSet.py
+++ DictionaryAndSet.py
@@ -19,16 +19,37 @@ dic= {
 
 dic["fullName"]= "Abu Rahat" # add new value
 
-print(dic)
-print(dic["age"])
-print(dic["score"]["oop"])
+# print(dic)
+# print(dic["age"])
+# print(dic["score"]["oop"])
 
-print(dic.keys())#print all keys
-print(len(dic.keys()))
+# print(dic.keys())#print all keys
+# print(len(dic.keys()))
 
-print(dic.values())#print all values
-print(dic.get("fullName"))#print keys value
+# print(dic.values())#print all values
+# print(dic.get("fullName"))#print keys value
 
-new_dic = {"city":"Dhaka"}#update old dic
-dic.update(new_dic)
-print(dic)
+# new_dic = {"city":"Dhaka"}#update old dic
+# dic.update(new_dic)
+# print(dic)
+
+
+# Set in Python*********
+
+set1 = {1,2,3,4,5,66,5,7,8,"rahat",1,2,5,4}
+set2 = {2,3,5,8,7,9,"abuRahat"}
+
+set1.add(100)#add value in set
+
+union = set1.union(set2) #union
+intersection = set.intersection(set2)
+
+print(union)
+print("Length of Union :", len(union))
+print(intersection)
+print("Length of intersection :", len(intersection))
+
+# set1.clear()#clear set1
+
+
+
