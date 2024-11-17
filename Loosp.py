72f4aff4b028e0e12d96fae244ec3e73f56e3694
AbuRahat889 <aburahatshaum889@gmail.com>
2024-11-17 23:38:23 +0600

    factorial number for loop

--- Loosp.py
+++ Loosp.py
@@ -46,9 +46,14 @@
 
 n = 4
 fac = 1
-i = 1
+# i = 1
 
-while i<=n:
-    fac *=i
-    i+=1
-print("factorial number : " , fac)
+# while i<=n:
+#     fac *=i
+#     i+=1
+# print("factorial number : " , fac)
+
+for i in range(1, n+1):
+    fac *= i
+    i += 1
+print(fac)
