311ec1afdaa4802b03e5d2698166f41ab3da61cc
AbuRahat889 <aburahatshaum889@gmail.com>
2024-11-17 23:45:11 +0600

    solve error

--- Loosp.py
+++ Loosp.py
@@ -1,59 +1,59 @@
 
 #   qs 1
-# i = 0
-# while i<= 10:
-#     # print(i)
-#     i+=1
+i = 0
+while i<= 10:
+    # print(i)
+    i+=1
 
 # qs 2 (multipication table) 
-# n=5
-# while i<= 10:
-#     multiple = n * i
-#     print("the multiple are:" , multiple)
-#     i+=1
+n=5
+while i<= 10:
+    multiple = n * i
+    print("the multiple are:" , multiple)
+    i+=1
 
 # qs 3
-# num = [1,4,9,16,25,36,49,64,81,100]
-
-# while i < len(num):
-#     print(num[i])
-#     i+=1
+num = [1,4,9,16,25,36,49,64,81,100]
+i = 0
+while i < len(num):
+    print(num[i])
+    i+=1
 
 
 # qs 4
-# i = 0
-# num = (1,4,9,16,25,36,49,64,81,100)
-# n = int(input("Enter a number: "))
+i = 0
+num = (1,4,9,16,25,36,49,64,81,100)
+n = int(input("Enter a number: "))
 
-# while i < len(num):
-#     if(num[i] == n):
-#         print("match the number in index : ",i)
-#         break
-#     else:
-#         print("Dont Match")
-#     i+=1
+while i < len(num):
+    if(num[i] == n):
+        print("match the number in index : ",i)
+        break
+    else:
+        print("Dont Match")
+    i+=1
 
 # qs 5 (sum of natural number)
 
-# n = 7
-# sum = 0
+n = 7
+sum = 0
 
-# for i in range(1, n+1):
-#     sum +=i
-# print(sum)
+for i in range(1, n+1):
+    sum +=i
+print(sum)
 
 # qs 6 (sum of factorial number )
 
 n = 4
 fac = 1
-# i = 1
+i = 1
 
-# while i<=n:
-#     fac *=i
-#     i+=1
-# print("factorial number : " , fac)
+while i<=n:
+    fac *=i
+    i+=1
+print("factorial number : " , fac)
 
-# for i in range(1, n+1):
-#     fac *= i
-#     i += 1
-# print(fac)
+for i in range(1, n+1):
+    fac *= i
+    i += 1
+print(fac)
