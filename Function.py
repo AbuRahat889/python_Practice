31005b1b4281ecaa541504e2d474a7a7b2248f6c
AbuRahat889 <aburahatshaum889@gmail.com>
2024-11-18 21:26:10 +0600

    check Even Odd

--- Function.py
+++ Function.py
@@ -5,6 +5,15 @@ def sum_number (a,b):
     print(int(sum))
     return sum
 
-a = float(input("Enter a number : "))
-b = float(input("Enter another number : "))
-sum_number(a,b)
+# a = float(input("Enter a number : "))
+# b = float(input("Enter another number : "))
+# sum_number(a,b)
+
+
+def even_odd(n):
+    if(n % 2 == 1 ):
+        print("Odd")
+    else:
+        print("Even")
+
+even_odd(15)
