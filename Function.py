5aa2f7c0d400d226a3b6739e50c74331176ec755
AbuRahat889 <aburahatshaum889@gmail.com>
2024-11-18 21:01:17 +0600

    create function

---
+++ Function.py
@@ -0,0 +1,10 @@
+
+
+def sum_number (a,b):
+    sum = a + b
+    print(int(sum))
+    return sum
+
+a = float(input("Enter a number : "))
+b = float(input("Enter another number : "))
+sum_number(a,b)
