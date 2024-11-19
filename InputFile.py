6d7ddba2b7e48949bfe2f0afd0956b8da586beb1
AbuRahat889 <aburahatshaum889@gmail.com>
2024-11-19 20:36:29 +0600

    write and append data

--- InputFile.py
+++ InputFile.py
@@ -3,4 +3,13 @@
 # read the file
 file = open("test.txt", "r")
 data = file.read()
+# print(data)
+file = open("test.txt", "w")
+file.write("this is new line.")# w change the all data
+ 
+# append data (append add the data at the end)
+file = open("test.txt", "a")
+file.write("\nthis is another new line")# w change the all data
+
 print(data)
+
